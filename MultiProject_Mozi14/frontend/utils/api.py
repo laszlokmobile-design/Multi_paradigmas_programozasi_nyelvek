21f9141db2645a2b49997625cdda00d96db04251
laszlokmobile-design <laszlo.k.mobile@gmail.com>
2025-12-16 22:01:37 +0100

    Update api.py

--- MultiProject_Mozi14/frontend/utils/api.py
+++ MultiProject_Mozi14/frontend/utils/api.py
@@ -11,6 +11,8 @@ import streamlit as st
 """
 API_BASE = st.secrets.get("API_BASE", "http://localhost:8000")#API címe
 
+BASE_URL = "https://multiparadigmasprogramozasinyelvek-cjjaqkrmg6z9t9jkybdtam.onrender.com"
+
 def _headers(): # token kezelés
     token = st.session_state.get("token")
     return {"Authorization": f"Bearer {token}"} if token else {}
@@ -28,4 +30,5 @@ def get_movies():
     return requests.get(f"{API_BASE}/movies/", headers=_headers())
 
 def add_movie(payload: dict):
+
     return requests.post(f"{API_BASE}/movies/", json=payload, headers=_headers())
