f786869eaad94ebe90485671f8262cae3544601f
laszlokmobile-design <laszlo.k.mobile@gmail.com>
2025-12-30 13:14:23 +0100

    Update database.py

--- MultiProject_Mozi14/backend/database.py
+++ MultiProject_Mozi14/backend/database.py
@@ -10,7 +10,13 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./movies.db")
 #2️⃣ SQLAlchemy engine létrehozása
 # SQLite esetén connect_args szükséges
 connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args,
+    pool_pre_ping=True
+)
+
 #3️⃣ Session létrehozása
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 #4️⃣ Base osztály
