1ddb04cb34485d75ead8b250eb18185d44b18bdb
laszlokmobile-design <laszlo.k.mobile@gmail.com>
2025-12-30 16:46:23 +0100

    Update database.py

--- MultiProject_Mozi14/backend/database.py
+++ MultiProject_Mozi14/backend/database.py
@@ -5,20 +5,17 @@ from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from dotenv import load_dotenv
 #1️⃣ Környezeti változók betöltése
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./movies.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 #2️⃣ SQLAlchemy engine létrehozása
 # SQLite esetén connect_args szükséges
 connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args=connect_args,
-    pool_pre_ping=True
-)
+engine = create_engine(DATABASE_URL)
 
 #3️⃣ Session létrehozása
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 #4️⃣ Base osztály
 Base = declarative_base()
 
