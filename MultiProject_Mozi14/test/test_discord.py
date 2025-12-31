4133b1aab3993885ee15d2495bd974e5e2dc2680
laszlokmobile-design <laszlo.k.mobile@gmail.com>
2025-12-31 18:03:59 +0100

    Create test_discord.py

---
+++ MultiProject_Mozi14/test/test_discord.py
@@ -0,0 +1,12 @@
+# backend/test_discord.py
+import os
+import requests
+
+print("Webhook URL:", os.getenv("DISCORD_WEBHOOK_URL"))
+
+url = os.getenv("DISCORD_WEBHOOK_URL")
+if url:
+    r = requests.post(url, json={"content": "Render teszt üzenet"})
+    print(r.status_code, r.text)
+else:
+    print("⚠️ DISCORD_WEBHOOK_URL nincs beállítva!")
