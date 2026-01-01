9177ce1a5f1e6508be5282da0a76ceb1afdbb52c
laszlokmobile-design <laszlo.k.mobile@gmail.com>
2026-01-01 09:45:25 +0100

    Delete MultiProject_Mozi14X directory

--- MultiProject_Mozi14X/frontend/components/navigation.py
+++
@@ -1,8 +0,0 @@
-# ==== frontend/components/navigation.py ====
-import streamlit as st
-
-
-def sidebar_menu():
-    return st.sidebar.radio(
-    "Menü", ["Belépés", "Filmek", "Új film", "Statisztika", "Profil"]
-    )
--- MultiProject_Mozi14X/start_backend.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export $(grep -v '^#' .env | xargs)
-uvicorn backend.main:app --host 0.0.0.0 --port 8000 --reload
