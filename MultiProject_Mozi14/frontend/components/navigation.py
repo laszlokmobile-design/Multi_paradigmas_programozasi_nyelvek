ba28ac212bf16ae7fc9d51722757577bf7e70291
laszlokmobile-design <laszlo.k.mobile@gmail.com>
2026-01-04 08:27:52 +0100

    Update navigation.py

--- MultiProject_Mozi14/frontend/components/navigation.py
+++ MultiProject_Mozi14/frontend/components/navigation.py
@@ -1,8 +1,14 @@
 # ==== frontend/components/navigation.py ====
 import streamlit as st
 
+# ======================================================
+# FUNKCIONÁLIS PROGRAMOZÁS
+# - Elkülönített, újrafelhasználható függvény
+# - Bemeneti adat (nincs explicit paraméter) alapján visszaadja a választott menüpontot
+# ======================================================
 
 def sidebar_menu():
     return st.sidebar.radio(
     "Menü", ["Belépés", "Filmek", "Új film", "Statisztika", "Profil"]
+
     )
